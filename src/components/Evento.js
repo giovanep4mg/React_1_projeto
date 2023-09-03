7f73cf4fb31bb03a0befce4397e3484dddbaa3d7
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-09-02 22:11:43 -0300

    atualizando depois da aula11

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,16 +1,21 @@
+import Button from './evento/Button';
 
-
-function Evento({numero}){
+function Evento(){
 
     function meuEvento(){
 
-        console.log(`Opa!Fui ativado número: ${numero}`);
+        console.log("Ativando primeiro evento! ");
+    }
+
+    function segundoEvento(){
+        console.log("Ativando segundo evento! ")
     }
 
     return(
         <div>
             <p>Clique aqui para disparar um evento:</p>
-            <button onClick={meuEvento}> Ativar </button>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo Evento" />
         </div>
     )
 }
