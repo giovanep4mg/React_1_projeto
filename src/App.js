654580d395f42d9f7a6852bcb796db1c95597aec
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-09-04 16:46:40 -0300

    aula13 adicionado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 function App() {
+
+  const meusItens =['React', 'vue', 'Angular']
 
     return (
 
     <div className="App">
 
-        <h1>Renderização condicional</h1>
-        <Condicional/>
+        <h1>Renderização de listas</h1>
+        <OutraLista itens={meusItens} />
+        <OutraLista itens={[]} />
+
     </div>
 
   );
