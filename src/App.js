fb762da89a23db2d767c49e1ea809979c9fb8d8d
giovanep4mg <giovaneteodoro79@gmail.com>
2023-09-03 21:58:38 -0300

    aula12 renderizacao por condicao

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-
-import Evento from './components/Evento';
-import Form from './components/Form';
+import Condicional from './components/Condicional';
 
 function App() {
 
@@ -9,12 +7,8 @@ function App() {
 
     <div className="App">
 
-        <h1>Testando Eventos</h1>
-
-        <Evento/>
-
-        <Form/>
-
+        <h1>Renderização condicional</h1>
+        <Condicional/>
     </div>
 
   );
